abac44ca3a7339bb1db5fc07411e5793316a3bc1
mariananbp <mariananbp@gmail.com>
2022-02-05 16:44:30 -0300

    route - create message

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,5 +1,8 @@
 const Router = require('express');
+const { messageCreate } = require('../controllers/messages.controller');
 
 const messagesRoute = new Router();
+
+messagesRoute.post('/', messageCreate);
 
 module.exports = { messagesRoute };
