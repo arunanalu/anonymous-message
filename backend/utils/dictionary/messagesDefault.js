6e4431f5c21eed14d7ca0987fd3dd8fe23dea6e6
mariananbp <mariananbp@gmail.com>
2022-02-05 18:25:09 -0300

    dictionary

--- backend/utils/dictionary/messagesDefault.js
+++ backend/utils/dictionary/messagesDefault.js
@@ -2,4 +2,6 @@ module.exports = {
   messageSent: 'Your message was sent',
   approvedMessage: 'This message was approved',
   messageNotFound: 'This message was not found',
+  messageNotDeleted: 'The message could not be deleted',
+  messageDeleted: 'This message was deleted',
 };
