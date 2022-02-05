bedffc11a653fd5b9f884e626b0a363cdf73858d
mariananbp <mariananbp@gmail.com>
2022-02-05 17:11:10 -0300

    route - get messages

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,8 +1,9 @@
 const Router = require('express');
-const { messageCreate } = require('../controllers/messages.controller');
+const { messageCreate, getAll } = require('../controllers/messages.controller');
 
 const messagesRoute = new Router();
 
 messagesRoute.post('/', messageCreate);
+messagesRoute.get('/', getAll);
 
 module.exports = { messagesRoute };
