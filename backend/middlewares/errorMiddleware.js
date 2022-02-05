dcc6afd283e61994faab866bba52a303d7a45844
mariananbp <mariananbp@gmail.com>
2022-02-05 18:00:47 -0300

    error middleware - return {message}

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -4,7 +4,7 @@
 const errorMiddleware = (error, req, res, next) => {
   if (error.status) {
     const { status, message } = error;
-    return res.status(status).json(message);
+    return res.status(status).json({ message });
   }
   console.log(error);
   return res.status(500).json({ message: 'Internal error' });
