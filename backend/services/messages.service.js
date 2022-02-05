3c4fe27f40f47484615e39c8dc1aec4925f235fb
mariananbp <mariananbp@gmail.com>
2022-02-05 17:10:33 -0300

    service - get messages

--- backend/services/messages.service.js
+++ backend/services/messages.service.js
@@ -1,10 +1,16 @@
-const { create } = require('../models/messages.model');
+const { create, findMessages } = require('../models/messages.model');
 
 const createMessage = async (anonymousMessage) => {
   const id = await create(anonymousMessage);
   return id;
 };
 
+const getMessages = async () => {
+  const messages = await findMessages();
+  return messages;
+};
+
 module.exports = {
   createMessage,
+  getMessages,
 };
