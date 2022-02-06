5382eed4890b6da6ef27298e365ddcd0ccedb002
mariananbp <mariananbp@gmail.com>
2022-02-05 21:19:41 -0300

    dictionary

--- backend/utils/dictionary/messagesDefault.js
+++ backend/utils/dictionary/messagesDefault.js
@@ -11,7 +11,6 @@ module.exports = {
   incorrectData: 'Incorrect username or password',
   allFields: 'All fields must be filled',
   jwtMalformed: 'jwt malformed',
-  recipeNotFound: 'recipe not found',
   missingAuth: 'missing auth token',
   onlyAdmins: 'Only admins can register new admins',
 };
